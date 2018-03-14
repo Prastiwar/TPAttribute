ff8a2acfa32b6c600dc311fad8678d6072f6cc48
Tomasz Piowczyk <tomasz.piowczyk@gmail.com>
2018-03-14 21:29:57 +0100

    Added Modifier functionality

--- TPAttribute.cs
+++ TPAttribute.cs
@@ -1,170 +1,117 @@
-﻿using System.Collections.Generic;
+﻿﻿using System.Collections.Generic;
 using UnityEngine;
 
 namespace TP.Utilities
 {
-    //[System.Serializable]
-    //public class TPFlatModifier : TPModifier
-    //{
-    //    public TPFlatModifier(ModifierType modifierType, ModifierCommand modifierCommand, float modifierValue) : base(modifierType, modifierCommand, modifierValue)
-    //    {
-    //    }
-    //}
+    public enum ModifierType
+    {
+        Flat,
+        Percentage
+    }
+
+    public enum ModifierCommand
+    {
+        Increase,
+        Decrease,
+        Multiply
+    }
+
     [System.Serializable]
     public class TPModifier
     {
-        public TPModifier(ModifierType modifierType, ModifierCommand modifierCommand, float modifierValue)
-        {
-            ModifyType = modifierType;
-            ModifyCommand = modifierCommand;
-            ModifyValue = modifierValue;
-        }
-        public enum ModifierType
-        {
-            Flat,
-            Percentage
-        }
-        public enum ModifierCommand
-        {
-            Increase,
-            Decrease,
-            Multiply
-        }
-        public ModifierType ModifyType;
-        public ModifierCommand ModifyCommand;
-        public float ModifyValue;
+        public ModifierType Type;
+        public ModifierCommand Command;
+        public float Value;
+        public int Priority;
 
-        public void Modify(ref float value, ref float tempValue)
+        public TPModifier(ModifierType modifierType, ModifierCommand modifierCommand, float modifierValue, int modifierPriority)
         {
-            if (ModifyType == ModifierType.Percentage)
-            {
-                tempValue = Mathf.CeilToInt((value / ModifyValue) * 100);
-            }
-            switch (ModifyCommand)
-            {
-                case ModifierCommand.Increase:
-                    value = (ModifyType == ModifierType.Flat) ? (value += ModifyValue) : (value += tempValue);
-                    break;
-
-                case ModifierCommand.Decrease:
-                    value = (ModifyType == ModifierType.Flat) ? (value -= ModifyValue) : (value -= tempValue);
-                    break;
-
-                case ModifierCommand.Multiply:
-                    value = (ModifyType == ModifierType.Flat) ? (value *= ModifyValue) : (value *= tempValue);
-                    break;
-            }
+            Type = modifierType;
+            Command = modifierCommand;
+            Value = modifierValue;
+            Priority = modifierPriority;
         }
 
-        public void UnModify(ref float value, float tempValue = 0)
-        {
-            switch (ModifyCommand)
-            {
-                case ModifierCommand.Increase:
-                    value = ModifyType == ModifierType.Flat ? value -= ModifyValue : value -= tempValue;
-                    break;
-
-                case ModifierCommand.Decrease:
-                    value = ModifyType == ModifierType.Flat ? value += ModifyValue : value += tempValue;
-                    break;
-
-                case ModifierCommand.Multiply:
-                    value = ModifyType == ModifierType.Flat ? value /= ModifyValue : value /= tempValue;
-                    break;
-            }
-        }
+        public TPModifier(ModifierType modifierType, ModifierCommand modifierCommand, float modifierValue) : this(modifierType, modifierCommand, modifierValue, 0) { }
     }
 
-
-
-
-
-
+    [System.Serializable]
     public class TPAttribute
     {
-        public delegate void OnChangeHandler();
+        public float BaseValue { get; set; }
+        public float Value { get; set; }
+        public float MaxValue { get; set; }
 
         readonly List<TPModifier> _modifiers = new List<TPModifier>();
-        float _value;
-        float _maxValue;
-        float _baseValue;
 
-        float _tempValue;
+        public delegate void OnChangeHandler();
+
+        public OnChangeHandler OnChanged
+        {
+            get
+            {
+                if (OnChanged != null)
+                    return OnChanged;
+
+                return () => { };
+            }
+            set { OnChanged = value; }
+        }
 
         public void AddModifier(TPModifier modifier)
         {
             _modifiers.Add(modifier);
-            modifier.Modify(ref _value, ref _tempValue);
-            TPModifier mod = new TPModifier(TPModifier.ModifierType.Flat, TPModifier.ModifierCommand.Multiply, 1);
-            AddModifier(mod);
-            AddModifier(mod);
+            _modifiers.Sort(CompareModifiers);
+            Recalculate();
         }
 
         public void RemoveModifier(TPModifier modifier)
         {
             _modifiers.Remove(modifier);
-            modifier.UnModify(ref _value, _tempValue);
+            Recalculate();
         }
 
         public void RemoveAllModifiers()
         {
+            _modifiers.Clear();
+            Recalculate();
+        }
+
+        int CompareModifiers(TPModifier modifier1, TPModifier modifier2)
+        {
+            if (modifier1.Priority > modifier2.Priority)
+            {
+                return 1;
+            }
+            else if (modifier1.Priority < modifier2.Priority)
+            {
+                return -1;
+            }
+            return 0;
+        }
+
+        void Recalculate()
+        {
+            Value = BaseValue;
             foreach (var modifier in _modifiers)
             {
-                modifier.UnModify(ref _value, _tempValue);
-            }
-            _modifiers.Clear();
-        }
+                var modifyValue = modifier.Type == ModifierType.Flat ? modifier.Value : Mathf.CeilToInt((Value / modifier.Value) * 100);
 
-        public OnChangeHandler OnChange
-        {
-            get
-            {
-                if (OnChange != null)
-                    return OnChange;
-
-                return () => { };
+                switch (modifier.Command)
+                {
+                    case ModifierCommand.Increase:
+                        Value = Value += modifyValue;
+                        break;
+                    case ModifierCommand.Decrease:
+                        Value = Value -= modifyValue;
+                        break;
+                    case ModifierCommand.Multiply:
+                        Value = Value *= modifyValue;
+                        break;
+                }
             }
-            set { OnChange = value; }
+            OnChanged();
         }
-
-        public float BaseValue
-        {
-            get
-            {
-                return _baseValue;
-            }
-            set
-            {
-                _baseValue = value;
-                OnChange();
-            }
-        }
-
-        public float Value
-        {
-            get
-            {
-                return _value;
-            }
-            set
-            {
-                _value = value;
-                OnChange();
-            }
-        }
-
-        public float MaxValue
-        {
-            get
-            {
-                return _maxValue;
-            }
-            set
-            {
-                _maxValue = value;
-            }
-        }
-        
 
     }
 
